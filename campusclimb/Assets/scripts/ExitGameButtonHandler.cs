48e7748a60948e347388072dff21ff02ae8917c9
Annelie12 <yanliw@net.chalmers.se>
2023-02-04 06:54:38 +0100

    implemented exit button

---
+++ campusclimb/Assets/scripts/ExitGameButtonHandler.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGameButtonHandler : MonoBehaviour
+{
+    public void OnButtonClick()
+    {
+        Debug.Log("You have clicked the button");
+        Application.Quit();
+    }
+}   
